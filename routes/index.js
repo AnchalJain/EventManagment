d8802ab03be5d58d8f154d1581d5f6c9a1b047c7
AnchalJain <anchalj109@gmail.com>
2018-02-08 17:23:02 +0530

    manage authentication mechanisms

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var auth = require("../controller/AuthController.js");
+var auth = require("../controllers/AuthController.js");
 
-//restrict index for logged in user only
+// restrict index for logged in user only
 router.get('/', auth.home);
 
-//route to register page
+// route to register page
 router.get('/register', auth.register);
 
-//route for register action
+// route for register action
 router.post('/register', auth.doRegister);
 
-//route to login page
-router.get('/login',auth.login);
+// route to login page
+router.get('/login', auth.login);
 
-//route for login action
-router.post('/login',auth.doLogin);
+// route for login action
+router.post('/login', auth.doLogin);
 
-//route for logout action
+// route for logout action
 router.get('/logout', auth.logout);
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
